c82e25d6cb194f7c5d2c1ff32b9418542f4a72fe
Martijn Hoekstra <martijnhoekstra@gmail.com>
2013-11-02 18:08:22 +0100

    do some output

--- src/main/scala/memtester/Tester.scala
+++ src/main/scala/memtester/Tester.scala
@@ -9,6 +9,7 @@ object Tester {
         case heaps :: mems :: Nil => {
           val heap = heaps.toInt
           val mem = mems.toInt
+          if (heap > mem) println(s"more heap than mem? Impossibru! $heap > $mem")
           writeSuccess(heap, mem)
         }
         case _ => println("you're doing it wrong")
@@ -16,11 +17,12 @@ object Tester {
   }
     
     def writeSuccess(heap : Int, mem : Int) {
-      
+      println(s"we're running successfully with $heap heap and $mem mem")
       val r = new RedisClient("tools-redis", 6379)
       val got = r.get("javamemtest" + heap)
       if (got.isDefined )
       if (got.map(_.toInt).getOrElse(Int.MaxValue) > mem){
+        println(s"wrote out key javamemtest$heap")
         r.set("javamemtest" + heap, mem)
       }
     }
